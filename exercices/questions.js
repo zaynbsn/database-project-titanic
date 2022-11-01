3cddcbc355d7888d86d5a043af5f1cb3a91a194e
zaynbsn <nils.vainui@gmail.com>
2022-11-01 14:28:52 +0100

    added keypress waiting between questions and loop to define questions array

--- exercices/questions.js
+++ exercices/questions.js
@@ -1,44 +1,43 @@
-const questions = [
-  require('./questions/q1'),
-  require('./questions/q2'),
-  require('./questions/q3'),
-  require('./questions/q4'),
-  require('./questions/q5'),
-  require('./questions/q6'),
-  require('./questions/q7'),
-  require('./questions/q8'),
-  require('./questions/q9'),
-  require('./questions/q10'),
-  require('./questions/q11'),
-  require('./questions/q12'),
-  require('./questions/q13'),
-  require('./questions/q14'),
-  require('./questions/q15')
-]
+const questions = []
+for(let i=1; i<16; i++){
+  questions.push(require('./questions/q' + i))
+}
 
 module.exports = async function(client){
+  console.log("Hello! In this console, all the answers to the project's questions will be displayed".red)
+  await sleep(2000)
+  console.log('You can exit the program anytime by pressing "Ctrl+c"'.red)
+  await sleep(1500)
+  console.log('Press any key to see the first question'.red)
+  await keypress()
+  clear()
+  
   for (const question of questions){
     await question(client)
-    console.log('next question in 15 seconds'.red) 
-    await sleep(15000)
-    
-    
-    // console.log('waiting keypress..')
-    // await waitingKeypress();
+    console.log('press any key to see the next question...'.red)
+    await keypress()
+    clear()
   }
+  console.log('thank you for your lecture have a great time :)'.blue)
 }
 
-function sleep(ms) {
+const sleep = (ms) => {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
-// function waitingKeypress() {
-//   return new Promise((resolve) => {
-//     document.addEventListener('keydown', onKeyHandler);
-//     function onKeyHandler(e) {
-//       if (e.keyCode === 13) {
-//         document.removeEventListener('keydown', onKeyHandler);
-//         resolve();
-//       }
-//     }
-//   });
-// }
+
+const keypress = async () => {
+  process.stdin.setRawMode(true)
+  return new Promise(resolve => process.stdin.once('data', data => {
+    const byteArray = [...data]
+    if (byteArray.length > 0 && byteArray[0] === 3) {
+      console.log('^C')
+      process.exit(1)
+    }
+    process.stdin.setRawMode(false)
+    resolve()
+  }))
+}
+
+const clear = () => {
+  process.stdout.write('\033c')
+}
